66fe8e0c2e23b1c5db03e3e53afe7a68ba4d772f
Milaya78 <ela_keyt@mail.ru>
2024-12-12 20:58:25 +0100

    mongoDB

---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { getEnvVar } from './utils/getEnvVar.js';
+import contactsRouter from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+
+dotenv.config();
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      message: 'Welcome to the Contacts API!',
+    });
+  });
+
+  app.use(contactsRouter);
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
